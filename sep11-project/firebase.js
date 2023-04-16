35eb8b018e81366551806d13ae7cee22ec1d7ad7
jimingz9380 <91745086+jimingz9380@users.noreply.github.com>
2023-04-16 16:35:40 +0000

    Sun, Apr 16, 2023, 12:35 PM -04:00

--- sep11-project/firebase.js
+++ sep11-project/firebase.js
@@ -19,22 +19,22 @@
         const db = getDatabase(app);
 
 
-        // // custom code
-        // const numLikesDOM = document.querySelector('#numLikes');
-        // onValue(ref(db, "/likes/numLikes"), (snapshot) => {
-        //     let data = snapshot.val();
-        //     numLikesDOM.innerHTML = data
-        // });
+        // custom code
+        const numLikesDOM = document.querySelector('#numLikes');
+        onValue(ref(db, "/likes/numLikes"), (snapshot) => {
+            let data = snapshot.val();
+            numLikesDOM.innerHTML = data
+        });
 
-        // document.querySelector('button').addEventListener('click',function(){
-        //     // const numberLikes = parseInt(numLikes[i].innerHTML) + 1
+        document.querySelector('button').addEventListener('click',function(){
+            // const numberLikes = parseInt(numLikes[i].innerHTML) + 1
 
-        //     const numberLikes = parseInt(numLikesDOM.innerHTML) + 1;
-        //     set(ref(db, "likes"), {
-        //         numLikes: numberLikes,
-        //     });
+            const numberLikes = parseInt(numLikesDOM.innerHTML) + 1;
+            set(ref(db, "likes"), {
+                numLikes: numberLikes,
+            });
 
-        // });
+        });
 
         const allMessages = document.querySelector("#all-messages");
         const usernameElem = document.querySelector("#user-input");
