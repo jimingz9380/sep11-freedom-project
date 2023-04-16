f40aff6b18ce78439c580e0a53c8b2722242afaf
jimingz9380 <91745086+jimingz9380@users.noreply.github.com>
2023-04-16 15:20:00 +0000

    Sun, Apr 16, 2023, 11:20 AM -04:00

--- sep11-project/firebase.js
+++ sep11-project/firebase.js
@@ -34,43 +34,7 @@
                 numLikes: numberLikes,
             });
 
-
-
         });
 
-        const username = prompt("Please Tell Us Your Name");
 
-        function sendMessage(e) {
-        e.preventDefault();
-
-        // get values to be submitted
-        const timestamp = Date.now();
-        const messageInput = document.getElementById("message-input");
-        const message = messageInput.value;
-
-        // clear the input box
-        messageInput.value = "";
-
-        //auto scroll to bottom
-        document
-            .getElementById("messages")
-            .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-        // create db collection and send in the data
-        db.ref("messages/" + timestamp).set({
-            username,
-            message,
-        });
-        }
-
-        const fetchChat = db.ref("messages/");
-
-        fetchChat.on("child_added", function (snapshot) {
-        const messages = snapshot.val();
-        const message = `<li class=${
-            username === messages.username ? "sent" : "receive"
-        }><span>${messages.username}: </span>${messages.message}</li>`;
-        // append the message on the page
-        document.getElementById("messages").innerHTML += message;
-        });
 
