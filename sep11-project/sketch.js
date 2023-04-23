68867710ba60d941ef3b6160f37cf1e5ee78219e
jimingz9380 <91745086+jimingz9380@users.noreply.github.com>
2023-04-23 22:25:31 +0000

    Sun, Apr 23, 2023, 6:25 PM -04:00

--- sep11-project/sketch.js
+++ sep11-project/sketch.js
@@ -14,13 +14,16 @@
             background(35, 97, 69);
             //box circle, square, and trianle
             fill(0)
-            rect(0, 0, 100, 100);
-            rect(0, 100, 100, 100);
-            rect(0, 200, 100, 100);
+            rect(0, 0, 200, 200);
+            rect(0, 100, 200, 200);
+            rect(0, 200, 200, 200);
+            stroke(255)
+            line(0, 200, 100, 200)
 
             fill(255);
-            ellipse(50, 50, 25);
-            triangle(50, 150, 25, 175, 75, 175);
-            rect(0, 210, 75);
+            ellipse(50, 50, 50);
+            triangle(50, 125, 25, 175, 75, 175);
+            rect(50, 250, 75);
+            
 
         }
