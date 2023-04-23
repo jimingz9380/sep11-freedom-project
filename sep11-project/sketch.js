7018a62ec91de5019aabbd130b290aed8b68bb5d
jimingz9380 <91745086+jimingz9380@users.noreply.github.com>
2023-04-23 19:05:51 -0400

    redo

--- sep11-project/sketch.js
+++ sep11-project/sketch.js
@@ -8,11 +8,14 @@
         }
 
         let shapePicker = 0
-        mousexArr = {}
-        mouseyArr = {}
+        let limits = 1
+        mousexArr = []
+        mouseyArr = []
 
 
         function mousePressed(){
+            limits++
+            // console.log(mouseX, mouseY)
             if(mouseX > 0 && mouseX < 101 && mouseY > 0 && mouseY < 101 ){
                 shapePicker= 0
                 // console.log(0)
@@ -23,8 +26,13 @@
                 shapePicker = 2
                 // console.log(2)
             }
-            mousexArr.push()
-            console.log(mousexArr)
+
+            if(mouseX > 101 && mouseY > 0 && mouseY < 301){
+                mousexArr.push(mouseX)
+                mouseyArr.push(mouseY)
+                console.log(mouseX, mouseY)
+            }
+
         }
         function draw() {
             background(35, 97, 69);
@@ -41,14 +49,19 @@
             triangle(50, 125, 25, 175, 75, 175);
             rect(50, 250, 75);
 
-            if(mouseIsPressed && shapePicker == 0){
-                ellipse(mouseX, mouseY, 50);
+            for(let i = 1; i < limits; i++){
 
-            }else if(mouseIsPressed && shapePicker == 1){
-                triangle(mouseX, mouseY - 50, mouseX + 50, mouseY + 50, mouseX - 50, mouseY + 50)
-            }else if(mouseIsPressed && shapePicker == 2){
-                rectMode(CENTER);
-                rect(mouseX, mouseY, 50);
+                if(shapePicker == 0 && mouseX > 101){
+                    ellipse(mousexArr[i], mouseyArr[i], 50);
+                    // console.log(mouseX)
+                }
+                else if(shapePicker == 1 && mouseX > 101 && mouseY > 0 && mouseY < 301){
+                    triangle(mousexArr[i],  mouseyArr[i] - 50, mousexArr[i] + 50, mouseyArr[i] + 50, mousexArr[i] - 50,  mouseyArr[i] + 50)
+
+                }else if(shapePicker == 2 && mouseX > 101 && mouseY > 0 && mouseY < 301){
+                    rectMode(CENTER);
+                    rect(mousexArr[i],  mouseyArr[i], 50);
+                }
             }
 
         }
